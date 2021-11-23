7858b4a0fd83a6490b735aa77a98aa7234560b3a
Neil Seward <neil.seward@elasticpath.com>
2021-11-23 15:39:19 -0500

    Update

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ HelloWorld kopf Python Operator for Kubernetes
 
 1. Create the kind cluster.
 ```bash
-kind create cluster --config cluser.yaml
+kind create cluster --config cluster.yaml
 ```
 
 2. Create the kubectl config file.
@@ -20,10 +20,39 @@ kubectl cluster-info --context kind-hub
 
 3. Create the `DemoWeb` CustomResourceDefinition.
 ```bash
-kubectl apply -f crd.yaml
+kubectl apply -f crd-resources/crd.yaml
 ```
 
 4. Install kopf and kubernetes Python packages.
 ```bash
 pip3 install -r requirements.txt
+```
+
+5. Run the operator.
+```bash
+kopf run operator.py
+```
+
+6. Create the resources.
+```bash
+kubectl apply -f sealneaward-demoweb.yaml
+```
+
+### Access Deployment
+
+1. Get the service.
+```bash
+kubectl get svc
+```
+
+2. Port forward to the service.
+```
+kubectl port-forward svc/sealneaward-svc 8080:8080
+```
+
+### Delete Deployment
+
+1. Delete the resources.
+```bash
+kubectl delete -f sealneaward-demoweb.yaml
 ```
--- operator.py
+++ operator.py
@@ -2,10 +2,12 @@ import kopf
 import kubernetes.client
 from kubernetes.client.rest import ApiException
 import yaml
+import pystache
 
 @kopf.on.create('sealneaward.github', 'v1', 'demowebs')
 def create_fn(spec, **kwargs):
     name = kwargs["body"]["metadata"]["name"]
+    namespace = kwargs["body"]["metadata"]["namespace"]
     print("Name is %s\n" % name)
     # Create the deployment spec
     doc = yaml.safe_load(f"""
@@ -13,6 +15,7 @@ def create_fn(spec, **kwargs):
         kind: Deployment
         metadata:
           name: {name}-deployment
+          namespace: {namespace}
           labels:
             app: {name}
         spec:
@@ -57,7 +60,59 @@ def create_fn(spec, **kwargs):
     api = kubernetes.client.AppsV1Api()
     try:
       depl = api.create_namespaced_deployment(namespace=doc['metadata']['namespace'], body=doc)
-      # Update the parent's status.
-      return {'children': [depl.metadata.uid]}
     except ApiException as e:
       print("Exception when calling AppsV1Api->create_namespaced_deployment: %s\n" % e)
+
+    # Create the service spec
+    doc = yaml.safe_load(f"""
+        apiVersion: v1
+        kind: Service
+        metadata:
+          labels:
+            app: {name}
+          name: {name}-svc
+          namespace: {namespace}
+        spec:
+          ports:
+          - port: 8080
+            protocol: TCP
+            targetPort: 80
+          selector:
+            app: {name}
+          sessionAffinity: None
+          type: ClusterIP
+    """)
+
+    # Make it our child: assign the namespace, name, labels, owner references, etc.
+    kopf.adopt(doc)
+
+    # Actually create an object by requesting the Kubernetes API.
+    api = kubernetes.client.CoreV1Api()
+    try:
+      svc = api.create_namespaced_service(namespace=doc['metadata']['namespace'], body=doc)
+      # Update the parent's status.
+      return {'children': [svc.metadata.uid]}
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->create_namespaced_service: %s\n" % e)
+
+@kopf.on.delete('sealneaward.github', 'v1', 'demowebs')
+def delete_fn(spec, **kwargs):
+    name = kwargs["body"]["metadata"]["name"] + "-deployment"
+    namespace = kwargs["body"]["metadata"]["namespace"]
+    # Delete the deployment spec
+    # Actually delete an object by requesting the Kubernetes API.
+    api = kubernetes.client.AppsV1Api()
+    try:
+      depl = api.delete_namespaced_deployment(name=name, namespace=namespace)
+    except ApiException as e:
+      print("Exception when calling AppsV1Api->delete_namespaced_deployment: %s\n" % e)
+
+    name = kwargs["body"]["metadata"]["name"] + "-svc"
+    namespace = kwargs["body"]["metadata"]["namespace"]
+
+    # Actually delete an object by requesting the Kubernetes API.
+    api = kubernetes.client.CoreV1Api()
+    try:
+      svc = api.delete_namespaced_service(name=name, namespace=namespace)
+    except ApiException as e:
+      print("Exception when calling CoreV1Api->delete_namespaced_service: %s\n" % e)
